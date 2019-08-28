d3c83fb2f44eadf2615d1030ff136651d2e0803a
Ubuntu <ubuntu@ip-172-31-43-157.ec2.internal>
2019-08-28 12:00:47 +0000

    created a new app and included static and media folders

--- hawka_delight_project/urls.py
+++ hawka_delight_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from restaurant_app.views import show_index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', show_index)
 ]
