8465cf0a43ac4ea7a68d0cdb92ba748fc57f92a2
Ubuntu <ubuntu@ip-172-31-43-157.ec2.internal>
2019-09-05 03:07:27 +0000

    included testing models for checkout_app and submitrecipe_app

--- checkout_app/models.py
+++ checkout_app/models.py
@@ -8,11 +8,11 @@ class Order(models.Model):
     full_name = models.CharField(max_length=200, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
-    postcode = models.CharField(max_length=20, blank=True)
+    postcode = models.CharField(max_length=20, blank=False)
     town_or_city = models.CharField(max_length=40, blank=False)
     street_address1 = models.CharField(max_length=50, blank=False)
-    street_address2 = models.CharField(max_length=50, blank=False)
-    country = models.CharField(max_length=50, blank=False)
+    street_address2 = models.CharField(max_length=50, blank=True)
+    county = models.CharField(max_length=50, blank=True)
     date = models.DateField()
     
     def __str__(self):
--- checkout_app/tests.py
+++ checkout_app/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Order, OrderLineItem
+from restaurant_app.models import MenuItem
 
 # Create your tests here.
+
+# Tests for Order model
+class OrderTests(TestCase):
+    
+    def setUp(self):
+        Order.objects.create(full_name='test_name', phone_number='12345678', country='Singapore', postcode='123456', town_or_city='Singapore', street_address1='Building123', date='2011-11-12')
+        
+    def test_OrderTests(self):
+        test=Order.objects.get(full_name="test_name")
+        self.assertEqual(test.full_name, "test_name")
+        self.assertEqual(test.phone_number, "12345678")
+        self.assertEqual(test.country, "Singapore")
+        self.assertEqual(test.postcode, "123456")
+        self.assertEqual(test.town_or_city, "Singapore")
+        self.assertEqual(test.street_address1, "Building123")
+        
+
+# Tests for OrderLineItem model
+class OrderLineItemTests(TestCase):
+    def setUp(self):
+        order_test = Order.objects.create(full_name='test_name', phone_number='12345678', country='Singapore', postcode='123456', town_or_city='Singapore', street_address1='Building123', date='2011-11-12')
+        menu_test = MenuItem.objects.create(name='Chicken Rice', description='This is chicken rice.', price=2.10)
+        OrderLineItem.objects.create(order=order_test, item=menu_test, quantity='2')
+        
+    def test_OrderLineItemTests(self):
+        test=OrderLineItem.objects.get(quantity='2')
